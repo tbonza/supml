67767fc076239909489e83cf6f84f8ecdc81a3f0
tbonza <tylers.pile@gmail.com>
2018-12-13 07:38:06 -0500

    added functions related to visualizing bayesian classification of spatial features
    cleanup

--- R/bayes_utils.R
+++ R/bayes_utils.R
@@ -41,3 +41,106 @@ popmat <- function(xf,yf,z, m){
     }
     return(m)
 }
+
+#' Normalize a numeric object
+#'
+#' @param x numeric vector
+#' @return normalized values (0,1)
+#'
+#' @export
+normalize <- function(x) {(x - min(x)) / (max(x) - min(x))}
+
+#' Coordinate grid for plotting
+#'
+#' Matrix must be square
+#'
+#' @param m number of rows
+#' @param n number of columns
+#' @param step step size, ex. sqrt(m) / (1.0 / n)
+#'
+#' @export
+plotgrid <- function(m, n, step) {
+    
+    x <- 0.0
+    y <- 0.0
+    origin <- 0.0
+    groupid <- 1
+    
+    xcol <- c()
+    ycol <- c()
+    grp <- c()
+    
+    for (i in 1:m){
+        
+        for (j in 1:n){
+            
+            # make block
+            
+            if (x <= n * (1.0/m)){
+            xcol <- c(xcol, x, x, 
+                x + step, x + step)
+            ycol <- c(ycol, y,
+                     y+ step, y+ step,
+                     y)
+            grp <- c(grp, groupid, groupid,
+                    groupid, groupid)
+            }
+            # increment to next block          
+            if (n * step > y){
+                x <- x 
+                y <- y + step
+                
+                if (y + step > n * step){
+                    x <- x + step
+                    y <- origin
+                }
+                
+            } else {
+                x <- x + step
+                y <- origin
+            }
+            
+            groupid <- groupid + 1
+        }
+    }
+    df <- as.data.frame(grp)
+    df <- cbind(df, xcol, ycol)
+    return(df)
+}
+
+#' Convert matrix to visualization format
+#'
+#' This function is meant to be used when
+#' creating an overlaying layer when 
+#' generating maps in ggplot2. 
+#'
+#' @param X boolean square matrix
+#' @return dataframe containing group
+#'       information with factors
+#'
+#' @export
+matrix_group <- function(X){
+    grp <- 1
+    groups <- c()
+    values <- c()
+    for (i in 1:nrow(X)){
+        for (j in 1:ncol(X)){
+        
+            if (is.na(X[i,j]) == TRUE){
+                values <- c(values, "None")
+            }
+            else if (X[i,j] == TRUE){
+                values <- c(values, "True")
+            }
+            else {
+                values <- c(values, "False")
+            }
+            
+            groups <- c(groups, grp)
+            grp <- grp + 1
+        }
+    }
+    df <- as.data.frame(groups)
+    df <- cbind(df, values)
+    return(df)
+}
